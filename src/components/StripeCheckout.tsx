57f28226b51b6739d93f9f5945e83c5030d1b359
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-06-27 16:36:11 -0400

    completed stripe checkout form

--- src/components/StripeCheckout.tsx
+++ src/components/StripeCheckout.tsx
@@ -2,7 +2,7 @@
 // temporary disable to allow build to deploy to amplify
 import { useEffect, useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement, useStripe, Elements, useElements } from '@stripe/react-stripe-js'
+import { CardElement, useStripe, Elements, useElements, PaymentElement, LinkAuthenticationElement } from '@stripe/react-stripe-js'
 import axios from 'axios';
 // import { Navigate } from 'react-router-dom'; 
 import { useCartContext, useUserContext } from '../hooks';
@@ -11,56 +11,78 @@ import { useCartContext, useUserContext } from '../hooks';
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
-    const { cart, totalAmount, shippingFees } = useCartContext();
     const { myUser } = useUserContext();
     // stripe implementation here
     const [success, setSuccess] = useState(true);
     const [error, setError] = useState(null);
     const [processing, setProcessing] = useState(null);
     const [disabled, setDisabled] = useState(true);
-    const [clientSecret, setClientSecret] = useState("");
+    const [email, setEmail] = useState("");
+    const [isLoading, setIsLoading ] = useState(false);
+    const [ message, setMessage ] = useState("");
     const stripe = useStripe();
     const elements = useElements();
-    const cardStyle = {
-        style: {
-            base: {
-                color: "#32325d",
-                fontFamily: 'Arial, sans-serif',
-                fontSmoothing: 'antialiased',
-                fontSize: '16px',
-                '::placeholder': {
-                    color: '#32325d'
-                }, 
-            },
-            invalid: {
-                color: '#fa755a',
-                iconColor: '#fa755a'
-            },
-        }
-    }
-    const createPaymentIntent = async() => {
-        try {
-            await axios.post('https://f1kr0l3d4b.execute-api.us-east-1.amazonaws.com/test/create-payment-intent', {
-                cart, totalAmount, shippingFees
-            }).then(res => setClientSecret(res.data));
-        } catch(err) {
-            console.error(err);
-        }
+    const paymentElementOptions = {
+        layout: 'tabs',
     }
     useEffect(() => {
-        if (totalAmount) {
-            createPaymentIntent();
+        if (!stripe) {
+            return;
         }
-    }, [totalAmount])
+        const clientSecret = new URLSearchParams(window.location.search).get(
+            "payment_intent_client_secret"
+        );
+        if (!clientSecret) {
+            return;
+        }
+        stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+            switch (paymentIntent?.status) {
+                case "succeeded":
+                    setMessage("Payment succeeded!");
+                    break;
+                case "processing":
+                    setMessage("Your payment is processing.");
+                    break;
+                case "requires_payment_method":
+                    setMessage("Your payment was not successful, please try again.");
+                    break;
+                default:
+                    setMessage("Something went wrong.");
+                    break;
+            }
+        })
+    }, [stripe])
     // const handleChange = async(e) => {
 
     // }
-    // const handleSubmit = async(e) => {}
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            // disable form submission until stripe is loaded
+            return;
+        }
+
+        setIsLoading(true);
+        const { error } = await stripe.confirmPayment({
+            elements,
+            confirmParams: {
+                return_url: window.location.host
+            }
+        })
+
+        if (error.type === "card_error" || error.type === "validation_error") {
+            setMessage(error.message);
+        } else {
+            setMessage("An unexpected error occurred.");
+        }
+        setIsLoading(false);
+    }
     return (
         <div>
             <form className='stripe-form' id='payment-form'>
-                <CardElement id='card-element' options={cardStyle} />
-                <button disabled={processing || disabled || success} id='submit' className='stripe-button'>
+                <PaymentElement id="payment-element" options={paymentElementOptions} />
+                <button disabled={processing || disabled || success || isLoading } id='submit' className='stripe-button'>
                     <span id='button-text'>
                         {processing ? <div className='spinner'>Loading</div> : 'Pay'}
                     </span>
@@ -81,22 +103,42 @@ const CheckoutForm = () => {
 }
 
 const StripeCheckout = () => {
-    // IMPLEMENT THIS LATER
-//   const [ clientSecret, setClientSecret ] = useState("");
-//   useEffect(() => {
-//     axios.post("/create-payment-intent", {
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-//     })
-//     .then(res => setClientSecret(res.data.clientSecret));
-//   }, [])
+  const [ clientSecret, setClientSecret ] = useState("");
+  const { cart, totalAmount, shippingFees } = useCartContext();
+  const createPaymentIntent = async() => {
+  try {
+    await axios.post('https://f1kr0l3d4b.execute-api.us-east-1.amazonaws.com/test/create-payment-intent', {
+        cart, totalAmount, shippingFees
+    }).then(res => setClientSecret(res.data));
+  } catch(err) {
+     console.error(err);
+  }
+ }
+  useEffect(() => {
+    if (totalAmount) {
+        createPaymentIntent();
+    }
+  }, [totalAmount])
+  const appearance = {
+    theme: 'stripe',
+    variables: {
+        colorBackground: '#ffffff',
+        colorText: '#052e16',
+        colorPrimary: '#000000'
+    }
+  }
+  const options = {
+    clientSecret,
+    appearance
+  };
   return (
-    <div className="flex justify-center content-center h-full w-full">
-        <Elements stripe={stripePromise}>
-            <CheckoutForm />
-        </Elements>
-        
+    <div className="flex justify-center content-center h-full w-full my-8">
+        { clientSecret && (
+            <Elements options={options} stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
+            )
+        }
     </div>
   )
 } 
